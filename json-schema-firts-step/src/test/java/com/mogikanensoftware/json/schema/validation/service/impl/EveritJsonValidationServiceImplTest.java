d354eb0f0b5069f4718bb2b0810270406f6d8991
mogikanen9 <voinov9@gmail.com>
2016-10-17 16:04:52 -0400

    Message schema with validation and tests

--- json-schema-firts-step/src/test/java/com/mogikanensoftware/json/schema/validation/service/impl/EveritJsonValidationServiceImplTest.java
+++ json-schema-firts-step/src/test/java/com/mogikanensoftware/json/schema/validation/service/impl/EveritJsonValidationServiceImplTest.java
@@ -1,35 +1,29 @@
 package com.mogikanensoftware.json.schema.validation.service.impl;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.mogikanensoftware.json.schema.validation.service.AbstractTest;
 import com.mogikanensoftware.json.schema.validation.service.Request;
 import com.mogikanensoftware.json.schema.validation.service.Response;
 import com.mogikanensoftware.json.schema.validation.service.ValidationService;
 
-public class EveritJsonValidationServiceImplTest {
+public class EveritJsonValidationServiceImplTest extends AbstractTest{	
 
-	protected String readMainResource(String relativePath) throws IOException{
-		return new String(Files.readAllBytes(Paths.get(
-				EveritJsonValidationServiceImpl.class.getResource(relativePath).getPath())));
+	private String physicianSchema; 
+	private String messageSchema;
+		
+	public EveritJsonValidationServiceImplTest() throws IOException{
+		 physicianSchema = readResource("/json/schema/basic/physician.json");
+		 messageSchema = readResource("/json/schema/basic/message.json");
 	}
-	
-	protected String readTestReosurce(String relativePath) throws IOException{
-		return new String(Files.readAllBytes(Paths.get(
-				EveritJsonValidationServiceImpl.class.getResource(relativePath).getPath())));
-	}
-	
 	
 	@Test
 	public void testValidPhysicianJsonData() throws Exception {
-
-		String physicianSchema = readMainResource("/json/schema/basic/physician.json");
-
-		String physicianData = readTestReosurce("/json/data/basic/physician-sample-valid-1.json");
+	
+		String physicianData = readResource("/json/data/basic/physician-sample-valid-1.json");
 
 		Request request = new SimpleRequest(physicianData, physicianSchema);
 
@@ -39,13 +33,11 @@ public class EveritJsonValidationServiceImplTest {
 		Assert.assertTrue(response.isValid());
 
 	}
-	
+
 	@Test
-	public void testInvalidJsonDataMissingFirstName() throws Exception {
+	public void testInvalidJsonDataMissingFirstName() throws Exception {		
 
-		String physicianSchema = readMainResource("/json/schema/basic/physician.json");
-
-		String physicianData = readTestReosurce("/json/data/basic/physician-sample-invalid-no-firstname.json");
+		String physicianData = readResource("/json/data/basic/physician-sample-invalid-no-firstname.json");
 
 		Request request = new SimpleRequest(physicianData, physicianSchema);
 
@@ -62,9 +54,38 @@ public class EveritJsonValidationServiceImplTest {
 	@Test
 	public void testInvalidJsonDataMissingCpso() throws Exception {
 
-		String physicianSchema = readMainResource("/json/schema/basic/physician.json");
+		String physicianData = readResource("/json/data/basic/physician-sample-invalid-no-cpso.json");
 
-		String physicianData = readTestReosurce("/json/data/basic/physician-sample-invalid-no-cpso.json");
+		Request request = new SimpleRequest(physicianData, physicianSchema);
+
+		ValidationService service = new EveritJsonValidationServiceImpl();
+		Response response = service.validate(request);
+		Assert.assertNotNull(response);
+		Assert.assertFalse(response.isValid());
+		Assert.assertNotNull(response.getErrorMessages());
+		Assert.assertFalse(response.getErrorMessages().isEmpty());
+		response.getErrorMessages().forEach(System.out::println);
+
+	}
+
+	@Test
+	public void testValidPhysicianJsonData2() throws Exception {		
+
+		String physicianData = readResource("/json/data/basic/physician-sample-valid-2.json");
+
+		Request request = new SimpleRequest(physicianData, physicianSchema);
+
+		ValidationService service = new EveritJsonValidationServiceImpl();
+		Response response = service.validate(request);
+		Assert.assertNotNull(response);
+		Assert.assertTrue(response.isValid());
+
+	}
+
+	@Test
+	public void testInvalidJsonDataPracticeYear0() throws Exception {		
+
+		String physicianData = readResource("/json/data/basic/physician-sample-invalid-practice-years.json");
 
 		Request request = new SimpleRequest(physicianData, physicianSchema);
 
@@ -79,11 +100,9 @@ public class EveritJsonValidationServiceImplTest {
 	}
 	
 	@Test
-	public void testValidPhysicianJsonData2() throws Exception {
+	public void testValidPhysicianJsonDataWithClinic() throws Exception {		
 
-		String physicianSchema = readMainResource("/json/schema/basic/physician.json");
-
-		String physicianData = readTestReosurce("/json/data/basic/physician-sample-valid-2.json");
+		String physicianData = readResource("/json/data/basic/physician-sample-valid-with-clinic.json");
 
 		Request request = new SimpleRequest(physicianData, physicianSchema);
 
@@ -95,21 +114,30 @@ public class EveritJsonValidationServiceImplTest {
 	}
 	
 	@Test
-	public void testInvalidJsonDataPracticeYear0() throws Exception {
+	public void testValidMessageJsonData1() throws Exception {		
 
-		String physicianSchema = readMainResource("/json/schema/basic/physician.json");
+		String messageData = readResource("/json/data/basic/message-sample-valid-1.json");
 
-		String physicianData = readTestReosurce("/json/data/basic/physician-sample-invalid-practice-years.json");
+		Request request = new SimpleRequest(messageData, messageSchema);
 
-		Request request = new SimpleRequest(physicianData, physicianSchema);
+		ValidationService service = new EveritJsonValidationServiceImpl();
+		Response response = service.validate(request);
+		Assert.assertNotNull(response);
+		Assert.assertTrue(response.isValid());
+
+	}
+	
+	@Test
+	public void testInValidMessageJsonDataMIssingSFId() throws Exception {		
+
+		String messageData = readResource("/json/data/basic/message-sample-invalid-missing-sfid.json");
+
+		Request request = new SimpleRequest(messageData, messageSchema);
 
 		ValidationService service = new EveritJsonValidationServiceImpl();
 		Response response = service.validate(request);
 		Assert.assertNotNull(response);
 		Assert.assertFalse(response.isValid());
-		Assert.assertNotNull(response.getErrorMessages());
-		Assert.assertFalse(response.getErrorMessages().isEmpty());
-		response.getErrorMessages().forEach(System.out::println);
 
 	}
 }
